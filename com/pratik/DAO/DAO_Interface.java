269be7230c8d093e219d35ef718186b84863881b
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-08 15:06:20 +0530

    This is the Interface

---
+++ com/pratik/DAO/DAO_Interface.java
@@ -0,0 +1,11 @@
+package com.pratik.DAO;
+
+import java.util.List;
+
+import com.pratik.domain.product;
+
+public interface DAO_Interface {
+	public List<product> list();
+	public void restricions();
+
+}
